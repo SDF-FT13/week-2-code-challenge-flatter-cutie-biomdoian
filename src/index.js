10740778abe3b50c8f99901775a6f203d2cedfe1
Ian <iankruze60@gmail.com>
2025-03-22 16:59:24 +0300

    j.son server configuration, fix css character name overflow(monkey)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // Your code here
+const characterBar = document.getElementById('character-bar');
+const nameDisplay = document.getElementById('name');
+const imageDisplay = document.getElementById('image');
+const voteCountDisplay = document.getElementById('vote-count');
+const votesForm = document.getElementById('votes-form');
+const votesInput = document.getElementById('votes');
+const resetButton = document.getElementById('reset-btn');
+
+fetch('http://localhost:4000/characters') // The json-server I am using port 4000
+  .then(response => response.json())
+  .then(characters => {
+    characters.forEach(character => {
+      const characterButton = document.createElement('span');
+      characterButton.textContent = character.name;
+      characterBar.appendChild(characterButton);
+
+      characterButton.addEventListener('click', () => {
+        nameDisplay.textContent = character.name;
+        imageDisplay.src = character.image;
+        imageDisplay.alt = character.name;
+        voteCountDisplay.textContent = character.votes;
+        votesInput.value = ''; // Clears the input
+        selectedCharacter = character; //This code stores selected character globally for voting.
+      });
+    });
+  });
+
+let selectedCharacter = null; 
